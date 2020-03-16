f169e1b4b87cee9631635412cb976ab6bb79ad78
TrevorSalazar <tsala001@ucr.edu>
2020-03-16 08:11:15 -0700

    jcndlkjf

---
+++ heatmap.py
@@ -0,0 +1,45 @@
+import json
+from twython import Twython
+from twython import TwythonStreamer
+import pandas as pd
+import csv
+
+from config import *
+
+# Filter out unwanted data
+def process_tweet(tweet):
+    d = {}
+    d['hashtags'] = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
+    d['text'] = tweet['text']
+    d['user'] = tweet['user']['screen_name']
+    d['user_loc'] = tweet['user']['location']
+    return d
+    
+    
+# Create a class that inherits TwythonStreamer
+class MyStreamer(TwythonStreamer):     
+
+    # Received data
+    def on_success(self, data):
+
+        # Only collect tweets in English
+        if data['lang'] == 'en':
+            tweet_data = process_tweet(data)
+            self.save_to_csv(tweet_data)
+
+    # Problem with the API
+    def on_error(self, status_code, data):
+        print(status_code, data)
+        self.disconnect()
+        
+    # Save each tweet to csv file
+    def save_to_csv(self, tweet):
+        with open(r'saved_tweets.csv', 'a', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(list(tweet.values()))
+
+# Instantiate from our streaming class
+stream = MyStreamer(consumer_key, consumer_secret, 
+                    access_token, access_token_secret)
+# Start the stream
+stream.statuses.filter(track='ffxiv')
